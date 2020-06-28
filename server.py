a983f0c049db495569038458b6d44bdb30f55a69
oresun <ore312.kamaru@gmail.com>
2020-06-28 15:54:47 +0900

    ore312_ サーバー側のエントリーポイントを作成

--- server.py
+++ server.py
@@ -1,16 +1,22 @@
-
 import socket
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind(('127.0.0.1', 50000)) #50000 portを開く ip本番は0.0.0.0
-    s.listen(2)
-    while True:
-        conn, addr = s.accept() #接続したらコネクションとアドレスが入る
-        with conn:
-            while True:
-                data = conn.recv(20) #データを受け取る
-                if not data:
-                    break
-                print(data + "を受け取りました") #win or lose
-                
-                conn.sendall("あなたは" + data)
+#メイン関数
+def Main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(('127.0.0.1', 50000)) #50000 portを開く ip本番は0.0.0.0
+        s.listen(2)
+        while True:
+            conn, addr = s.accept() #接続したらコネクションとアドレスが入る
+            with conn:
+                while True:
+                    data = conn.recv(20) #データを受け取る
+                    if not data:
+                        break
+                    print(data + "を受け取りました") #win or lose
+                    
+                    conn.sendall("あなたは" + data)
+
+
+#エントリーポイント
+if __name__ == "__main__":
+    Main()
