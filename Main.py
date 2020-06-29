26c8c0da5194f1e6c02739ab6bca6af4f1a06776
oresun <ore312.kamaru@gmail.com>
2020-06-29 21:18:11 +0900

    Mainの改変

--- Main.py
+++ Main.py
@@ -2,17 +2,15 @@ import random           #乱数用
 import os               #コンソール操作用
 import Status as Stas   #自作モジュールのインポート
 
-
-# #定数群
-# NPC_NOB
-
-
-oda = {"name":"織田信長", "hp":10000, "atk":800, "def":400, "spe":200, "force":1200, "skill":2000, "point":300, "cost":50, "all":14700}
-hideyosi = {"name":"豊臣秀吉","hp":10000, "atk":500, "def":750, "spe":375, "force":1250, "skill":1750, "point":300, "cost":57, "all":14550}
-ieyasu = {"name":"徳川家康", "hp":10000, "atk":700, "def":350, "spe":175, "force":1050, "skill":1750, "point":300, "cost":57, "all":14150}
-imakawa = {"name","今川義元", "hp":10000, "atk":400, "def":600, "spe"300, "force":1000, "skill":1400, "point":300, "cost":71, "all":13700}
-singen ={"name":"武田信玄", "hp":10000, "atk":600, "def":300, "spe":150, "force":900,  "skill":1500, "point":300, "cost":66, "all":136000}
-aketi = {"neme":"明智光秀", "hp":10000, "atk":900, "def":450, "spe":225, "force":1350, "skill":2250, "point":300, "cost":44, "all":15250}
+#定数群
+NPC_CHARS = [
+    {"Id":1, "Name":"織田信長", "Hp":10000, "Atk":800, "Def":400, "Spe":200, "Force":1200, "Skill":2000, "point":300, "Cost":50, "All":14700 },
+    {"Id":2, "Name":"豊臣秀吉", "Hp":10000, "Atk":500, "Def":750, "Spe":375, "Force":1250, "Skill":1750, "point":300, "Cost":57, "All":14550 },
+    {"Id":3, "Name":"徳川家康", "Hp":10000, "Atk":700, "Def":350, "Spe":175, "Force":1050, "Skill":1750, "point":300, "Cost":57, "All":14150 },
+    {"Id":4, "Name":"今川義元", "Hp":10000, "Atk":400, "Def":600, "Spe":300, "Force":1000, "Skill":1400, "point":300, "Cost":71, "All":13700 },
+    {"Id":5, "Name":"武田信玄", "Hp":10000, "Atk":600, "Def":300, "Spe":150, "Force":900,  "Skill":1500, "point":300, "Cost":66, "All":136000},
+    {"Id":6, "Neme":"明智光秀", "Hp":10000, "Atk":900, "Def":450, "Spe":225, "Force":1350, "Skill":2250, "point":300, "Cost":44, "All":15250 },
+]
 
 #グローバル変数群
 gPlayer = Stas.clsStatus()
@@ -21,12 +19,28 @@ gPlayer = Stas.clsStatus()
 def dispClear():
     os.system("cls")
 
+def dispChars():
+    print("********************")
+    print("--------------------")
+    print("武将選択（数字）")
+    print("--------------------")
+    for i in range(len(NPC_CHARS)):
+        print(str(NPC_CHARS[i].get("Id")) + "." + NPC_CHARS[i].get("Name"))
+
+    print("********************")
+
+def selectChar():
+    dispChars()
+
 #メイン関数
 def Main():
-    dispClear()
+    selectChar()
+
+    print(NPC_CHARS)
+    # dispClear()
     
-    gPlayer.setDatas("aa", 1, 2, 3)
-    gPlayer.dispDatas()
+    # gPlayer.setDatas("aa", 1, 2, 3)
+    # gPlayer.dispDatas()
 
 #エントリーポイント
 if __name__ == "__main__":
